df69d07a50ccd444eda6216eea1daf3ca57dcd0c
Thomarkey <thoma_cz@hotmail.com>
2025-02-07 11:22:08 +0100

    added non-kata algo exercise

--- src/main/java/org/example/nonkata/MakeDuplicatesUniqueWithRange.java
+++ src/main/java/org/example/nonkata/MakeDuplicatesUniqueWithRange.java
@@ -31,28 +31,86 @@ public class MakeDuplicatesUniqueWithRange {
 
     public static void main(String[] args) {
         Set<String> uniqueStrings = new HashSet<>();
-        System.out.println(updateIfNeeded(uniqueStrings, "200")); //200
-        System.out.println(updateIfNeeded(uniqueStrings, "200")); //A00
-        System.out.println(updateIfNeeded(uniqueStrings, "C00")); //C00
-        System.out.println(updateIfNeeded(uniqueStrings, "C00")); //B00
-        System.out.println(updateIfNeeded(uniqueStrings, "C00")); //null
+        System.out.println(updateIfNeeded(uniqueStrings, "200", 0)); //200
+        System.out.println(updateIfNeeded(uniqueStrings, "200", 0)); //A00
+        System.out.println(updateIfNeeded(uniqueStrings, "C00", 0)); //C00
+        System.out.println(updateIfNeeded(uniqueStrings, "C00", 0)); //B00
+        System.out.println(updateIfNeeded(uniqueStrings, "C00", 0)); //null
 
     }
 
+//    public static String updateIfNeeded(Set<String> uniqueList, String input) {
+//        char[] prefixes = {'A', 'B', 'C'};
+//
+//        int prefixIndex = 0;
+//        while (!uniqueList.add(input)) {
+//            if (prefixIndex >= prefixes.length) {
+//                System.out.println("Unable to make unique string for:" + input);
+//                return null;
+//            }
+//            input = prefixes[prefixIndex] + input.substring(1);
+//            prefixIndex++;
+//        }
+//        return input;
+//    }
 
-    public static String updateIfNeeded(Set<String> uniqueList, String input) {
-        char[] prefixes = {'A', 'B', 'C'};
+    private final static char[] chars = {'A', 'B', 'C'};
 
-        int prefixIndex = 0;
-        while (!uniqueList.add(input)) {
-            if (prefixIndex >= prefixes.length) {
-                System.out.println("Unable to make unique string for:" + input);
-                return null;
-            }
-            input = prefixes[prefixIndex] + input.substring(1);
-            prefixIndex++;
+//    public static String updateIfNeeded(Set<String> uniqueList, String input, int charIndex) {
+//        if (!uniqueList.add(input)) {
+//            if (charIndex >= chars.length) {
+//                System.out.println("Unable to make unique string for:" + input);
+//                return null;
+//            }
+//            input = chars[charIndex] + input.substring(1);
+//            return updateIfNeeded(uniqueList, input, charIndex + 1);
+//        }
+//        return input;
+//    }
+
+    public static String updateIfNeeded(Set<String> uniqueList, String input, int charIndex) {
+        if (uniqueList.add(input)) {
+            return input;
         }
-        return input;
+        if (charIndex >= chars.length) {
+            System.out.println("Unable to make unique string for:" + input);
+            return null;
+        }
+        input = chars[charIndex] + input.substring(1);
+        return updateIfNeeded(uniqueList, input, charIndex + 1);
     }
-
 }
+
+
+//OTHER CHECKS
+
+//    public static void main(String[] args) throws Exception {
+//        System.out.println(setPost("ab")); //0ab
+//        System.out.println(setPost("b")); //00b
+//        System.out.println(setPost("a0b")); //a0b
+//        System.out.println(setPost("12")); //012
+//        System.out.println(setPost("123")); //123
+//        System.out.println(setPost("1234")); //errorr
+
+//        System.out.println(firstCharsForPost());
+
+//    }
+
+
+//    public static String setPost(String post) throws Exception {
+//        if (post.length() > 3) {
+//            throw new Exception("Post can not be larger than 3 chars!");
+//        }
+//        if (post.matches("\\d+")) {
+//            return String.format("%03d", Integer.parseInt(post));
+//        }
+//        return String.format("%3s", post).replace(' ', '0');
+//    }
+
+
+//    public static char[] firstCharsForPost() {
+//        String str = "A,B,C";
+//        return str.replaceAll("[,\\s]", "").toCharArray();
+//    }
+
+
